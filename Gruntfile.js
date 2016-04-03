a50a017df8a3ac536ea95cf8227cb17308b21b36
ChrisIngoeTlc <Christopher.Ingoe@thomsonlocal.com>
2016-04-03 23:08:49 +0100

    Grunt script; add build packages

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,55 @@ module.exports = function (grunt) {
             styles: 'styles',
             downloads: 'downloads',
             components: 'bower_components',
-            environment: environment
+            environment: environment,
+            fonts: 'font'
+        },
+        connect: {
+            options: {
+                port: 9000,
+                // Change this to '0.0.0.0' to access the server from outside.
+                hostname: 'localhost',
+                livereload: 35729
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    base: [
+                        '<%= config.tmp %>',
+                        '<%= config.app %>'
+                    ]
+                }
+            },
+            dist: {
+                options: {
+                    base: '<%= config.dist %>'
+                }
+            }
+        },
+        watch: {
+            js: {
+                files: ['<%= config.app %>/<%= config.scripts %>/{,**/}*.js'],
+                options: {
+                    livereload: true
+                }
+            },
+            compass: {
+                files: ['<%= config.app %>/<%= config.styles %>/{,**/}*.{scss,sass}'],
+                tasks: ['compass:server', 'autoprefixer']
+            },
+            gruntfile: {
+                files: ['Gruntfile.js']
+            },
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    '<%= config.app %>/{,**/}*.html',
+                    '<%= config.tmp %>/<%= config.styles %>/{,**/}*.css',
+                    '<%= config.app %>/<%= config.images %>/{,**/}*.{png,jpg,jpeg,gif,webp,svg}'
+                ]
+            }
         },
         clean: {
             server: '<%= config.tmp %>',
@@ -127,7 +175,7 @@ module.exports = function (grunt) {
             distTemplates: {
                 expand: true,
                 cwd: '<%= config.app %>/<%= config.views %>/',
-                dest: '<%= config.dist %>//<%= config.views %>/',
+                dest: '<%= config.tmp %>/<%= config.views %>/',
                 src: ['**']
             },
             images: {
@@ -138,8 +186,8 @@ module.exports = function (grunt) {
             },
             font: {
                 expand: true,
-                cwd: '<%= config.app %>/<%= config.font %>/',
-                dest: '<%= config.dist %>/<%= config.font %>/',
+                cwd: '<%= config.app %>/<%= config.fonts %>/',
+                dest: '<%= config.dist %>/<%= config.fonts %>/',
                 src: ['**']
             },
             scripts: {
@@ -172,6 +220,105 @@ module.exports = function (grunt) {
                 dest: '<%= config.build %>/production',
                 src: ['**']
             }
+        },
+        html2js: {
+            options: {
+                base: 'app',
+                module: 'appTemplates',
+                singleModule: true,
+                useStrict: true,
+                htmlmin: {
+                    collapseBooleanAttributes: true,
+                    collapseWhitespace: true,
+                    removeAttributeQuotes: true,
+                    removeComments: true,
+                    removeEmptyAttributes: true,
+                    removeRedundantAttributes: true,
+                    removeScriptTypeAttributes: true,
+                    removeStyleLinkTypeAttributes: true
+                }
+            },
+            main: {
+                src: ['.tmp/views/**/*.html'],
+                dest: '<%= config.tmp %>/scripts/appTemplates.js'
+            }
+        },
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: [
+                    '<%= config.tmp %>/<%= config.scripts %>/bower_comp.js',
+                    '<%= config.tmp %>/<%= config.scripts %>/config.js',
+                    '<%= config.tmp %>/<%= config.scripts %>/app.js',
+                    '<%= config.tmp %>/<%= config.scripts %>/appTemplates.js',
+                    '<%= config.tmp %>/<%= config.scripts %>/controllers/*.js',
+                    '<%= config.tmp %>/<%= config.scripts %>/rest/*.js',
+                    '<%= config.tmp %>/<%= config.scripts %>/services/*.js'
+                ],
+                dest: '<%= config.dist %>/<%= config.scripts %>/app.js'
+            }
+        },
+        ngAnnotate: {
+            options: {
+                singleQuotes: true,
+                separator: ';'
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= config.dist %>/<%= config.scripts %>',
+                    src: 'app.js',
+                    dest: '<%= config.dist %>/<%= config.scripts %>'
+                }]
+            }
+        },
+        uglify: {
+            options: {
+                mangle: false
+            },
+            dist: {
+                files: {
+                    '<%= config.dist %>/<%= config.scripts %>/app.js': ['<%= config.dist %>/<%= config.scripts %>/app.js']
+                }
+            }
+        },
+        cssmin: {
+            dist: {
+                files: {
+                    '<%= config.dist %>/<%= config.styles %>/main.css': ['<%= config.dist %>/<%= config.styles %>/main.css']
+                }
+            }
+        },
+        rev: {
+            dist: {
+                files: {
+                    src: [
+                        '<%= config.dist %>/scripts/{,*/}*.js',
+                        '<%= config.dist %>/styles/{,*/}*.css'
+                    ]
+                }
+            }
+        },
+        usemin: {
+            html: ['<%= config.dist %>/index.html'],
+            options: {
+                assetsDirs: ['<%= config.dist %>']
+            }
+        },
+        wiredep: {
+            task: {
+                src: ['<%= config.app %>/index.html']
+            }
+        },
+        bower_concat: {
+            all: {
+                dest: {
+                    'js': '<%= config.tmp %>/<%= config.scripts %>/bower_comp.js',
+                    'css': '<%= config.tmp %>/<%= config.styles %>/bower_comp.css'
+                }
+            }
         }
     });
 
@@ -187,7 +334,9 @@ module.exports = function (grunt) {
         grunt.task.run([
             'taskCleanWorkingDirs',
             'ngconstant:qa',
-            'taskBuildCode'
+            'taskBuildCode',
+            'clean:distQa',
+            'copy:distQa'
         ]);
     });
 
@@ -195,7 +344,9 @@ module.exports = function (grunt) {
         grunt.task.run([
             'taskCleanWorkingDirs',
             'ngconstant:development',
-            'taskBuildCode'
+            'taskBuildCode',
+            'clean:distDevelopment',
+            'copy:distDevelopment'
         ]);
     });
 
@@ -203,7 +354,9 @@ module.exports = function (grunt) {
         grunt.task.run([
             'taskCleanWorkingDirs',
             'ngconstant:production',
-            'taskBuildCode'
+            'taskBuildCode',
+            'clean:distProduction',
+            'copy:distProduction'
         ]);
     });
 
@@ -211,45 +364,56 @@ module.exports = function (grunt) {
         grunt.task.run([
             'taskCleanWorkingDirs',
             'ngconstant:staging',
-            'taskBuildCode'
+            'taskBuildCode',
+            'clean:distStaging',
+            'copy:distStaging'
         ]);
     });
 
     grunt.registerTask('taskBuildCode', function () {
         grunt.task.run([
+            'wiredep',
+            'bower_concat',
             'compass',
             'autoprefixer',
             'concat_css',
+            'copy:distTemplates',
+            'html2js',
+            'copy:scripts',
+            'concat',
+            'ngAnnotate',
+            'uglify',
             'copy:styles',
             'copy:html',
-            'copy:distComponents',
-            'copy:distTemplates',
             'copy:images',
             'copy:font',
-            'copy:scripts',
-            //'useminPrepare' //do no use
-            //'concat',
-            //'ngAnnotate',
-            //'uglify',
-            //'cssmin',
-            //'rev',
-            //'usemin',
-            //'clean:distBower',
-            //'clean:server',
-            //'clean:sasscache'
+            'cssmin',
+            'rev',
+            'usemin',
+            'clean:distBower',
+            'clean:server',
+            'clean:sasscache'
         ]);
     });
 
     grunt.registerTask('build', function () {
         grunt.task.run([
             'clean:build',
-            //'buildProduction',
-            //'buildStaging',
-            //'buildQa',
+            'buildProduction',
+            'buildStaging',
+            'buildQa',
             'buildDev'
-            //'clean:dist',
-            //'clean:server',
-            //'clean:sasscache'
+        ]);
+    });
+
+    grunt.registerTask('serve', function () {
+        grunt.task.run([
+            'taskCleanBasic',
+            'ngconstant:development',
+            'compass',
+            'autoprefixer',
+            'connect:livereload',
+            'watch'
         ]);
     });
 };
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
-angular.module('app', [])
+angular.module('app', ['ui.router'])
 
-    .run(['$rootScope', '$state', '$stateParams', function ($rootScope,   $state,   $stateParams) {
+    .run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
         $rootScope.$state = $state;
         $rootScope.$stateParams = $stateParams;
         //$rootScope.$on('$stateChangeError', function () {
